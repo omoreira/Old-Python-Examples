1e1b68d0a3a660c0150fa7aeb19df468c729b896
omsome <olga.moreira@gmail.com>
2018-03-11 15:58:25 -0400

    Update MLP_regression_10foldcv_boston.py

--- NN_examples/MLP_regression_10foldcv_boston.py
+++ NN_examples/MLP_regression_10foldcv_boston.py
@@ -1,6 +1,6 @@
 # MLP Regression 10-fold CV example using Boston dataset.
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 from sklearn import neural_network 
 from sklearn import datasets
 from sklearn.metrics import mean_squared_error, explained_variance_score
@@ -16,8 +16,9 @@ y = boston.target
 #x_train, x_test, y_train, y_test= cross_validation.train_test_split(x, y, test_size=0.2, random_state=42)
 # ######################################################################
 # Fit regression model
+n_fig=0
 for name, nn_unit in [
-        ('MLP using ReLU', neural_network.MLPRegressor(activation='relu', solver='lbfgs')), ('MLP using Logistic Neurons', neural_network.MLPRegressor(activation='logistic',solver='lbfgs')), ('MLP using TanH Neurons', neural_network.MLPRegressor(activation='tanh',solver='lbfgs'))
+        ('MLP using ReLU', neural_network.MLPRegressor(activation='relu', solver='lbfgs')), ('MLP using Logistic Neurons', neural_network.MLPRegressor(activation='logistic')), ('MLP using TanH Neurons', neural_network.MLPRegressor(activation='tanh',solver='lbfgs'))
         ]:
     regressormodel=nn_unit.fit(x,y)
     # Y predicted values
@@ -29,5 +30,12 @@ for name, nn_unit in [
     print('Method: %s' %name)
     print('RMSE on the data: %.4f' %rmse)
     print('RMSE on 10-fold CV: %.4f' %rmsecv)
-
-
+    n_fig=n_fig+1
+    pl.figure(n_fig)
+    pl.plot(yp, y,'ro')
+    pl.plot(yp_cv, y,'bo', alpha=0.25, label='10-folds CV')
+    pl.xlabel('predicted')
+    pl.title('Method: %s' %name)
+    pl.ylabel('real')
+    pl.grid(True)
+    pl.show()
